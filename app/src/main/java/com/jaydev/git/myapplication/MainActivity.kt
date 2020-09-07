aa40bdfff3cdcf4d5cd51ff0fa666a347636c657
JeongUk-Jay <jaydev.and@gmail.com>
2020-09-07 17:40:23 +0900

    init project

---
+++ app/src/main/java/com/jaydev/git/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jaydev.git.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
